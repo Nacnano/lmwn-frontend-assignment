ba23ef6f537a1f5cc479788fcc8c66421e9c5ba0
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-14 22:01:02 +0700

    docs: add comments for tsconfig error

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 // import Landing from "@pages/Landing";
-// NOTE: The alias was not working in build. Maybe because of the the way the build is configured.
-// No time to investigate T_T
+// NOTE: The "@" alias was not working in build. Maybe because of the the way the build is configured.
+// Will investigate (After finish with the main requirements T_T)
 
 function App() {
   return (
