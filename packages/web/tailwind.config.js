ea24d584f430d735581727393808105faf35c914
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-15 22:05:06 +0700

    style: add Prompt font

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
+    fontFamily: {
+      Prompt: ['Prompt', 'sans-serif']
+    },
     extend: {}
   },
   variants: {
