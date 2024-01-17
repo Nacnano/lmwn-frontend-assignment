58b2629e043ac11961696eaec60f908628fe7e8f
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-17 08:48:29 +0700

    style: flex on menu detail

--- packages/web/src/components/MenuDetails.tsx
+++ packages/web/src/components/MenuDetails.tsx
@@ -24,51 +24,59 @@ const MenuDetails = ({ menuName }: MenuDetailsProps) => {
   return (
     <>
       <div
-        className={`border p-4 rounded-md mb-4  transition flex flex-col items-start justify-between
+        className={`border rounded-md mb-4  transition  items-start justify-between
         ${inStock ? "hover:cursor-pointer hover:shadow-lg hover:bg-gray-50 shadow-md" : "z-0 cursor-default"}`}
         onClick={handleModal}
       >
-        <div className={` ${inStock === 0 ? "opacity-50" : ""}`}>
-          <h2 className="text-xl font-bold mb-2">{menu?.name}</h2>
-          <p className="text-gray-700">{menu?.fullPrice}฿</p>
-          {menu?.thumbnailImage && (
-            <img
-              src={menu?.thumbnailImage}
-              alt={menu?.name}
-              className="mt-2 rounded-md"
-              style={{ maxWidth: "100%" }}
-            />
-          )}
-          {menu?.discountedPercent > 0 && (
-            <p className="mt-2 text-green-600">
-              Discount: {menu?.discountedPercent}% off
-            </p>
-          )}
-
-          {menu?.discountedTimePeriod && (
-            <p className="text-gray-600">
-              Discount valid from {menu?.discountedTimePeriod.begin} to{" "}
-              {menu?.discountedTimePeriod.end}
-            </p>
-          )}
-
-          {menu?.options && (
+        <div className={`w-full h-full ${inStock === 0 ? "opacity-50" : ""}`}>
+          {menu?.largeImage ? (
             <div>
-              <h3 className="text-md font-medium my-2">See Options</h3>
+              <img
+                src={menu?.largeImage}
+                alt={menu?.name}
+                className="mb-4 rounded-t-md w-full h-40 object-cover"
+                style={{ maxWidth: "100%" }}
+              />
             </div>
-          )}
-        </div>
-
-        <div>
-          {inStock ? (
-            <p className="mt-2 text-gray-600"> {inStock} Left</p>
           ) : (
-            <div className="bg-red-100 text-red-300 font-bold text-xl py-3 rounded-md">
-              <p className="text-center">OUT OF ORDER</p>
+            <div className="bg-gray-100 rounded-md w-full h-40 flex justify-center items-center">
+              No Image
             </div>
           )}
-        </div>
 
+          <div className="px-4 flex flex-col justify-between h-auto">
+            <h2 className="text-xl font-bold mb-2">{menu?.name}</h2>
+
+            {menu?.discountedPercent > 0 && (
+              <p className="mt-2 text-green-600">
+                Discount: {menu?.discountedPercent}% off
+              </p>
+            )}
+
+            {menu?.discountedTimePeriod && (
+              <p className="text-gray-600">
+                Discount valid from {menu?.discountedTimePeriod.begin} to{" "}
+                {menu?.discountedTimePeriod.end}
+              </p>
+            )}
+
+            <div className="my-2 flex justify-between">
+              <p className="text-gray-800 text-left text-xl">
+                {menu?.fullPrice}฿
+              </p>
+
+              {inStock ? (
+                <p className="text-gray-600 text-right text-xl">
+                  {inStock} Left
+                </p>
+              ) : (
+                <div className="bg-red-100 text-red-300 font-semibold text-sm px-2 py-1 rounded-md">
+                  <p className="text-center">OUT OF ORDER</p>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
         <MenuModal isOpen={isModalOpen} menu={menu} />
       </div>
     </>
