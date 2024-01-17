4526024af559d9517005f308613cf6fa9f8f32a5
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-18 01:48:35 +0700

    feat: add back button

--- packages/web/src/pages/RestaurantPage.tsx
+++ packages/web/src/pages/RestaurantPage.tsx
@@ -1,4 +1,5 @@
 import { MoonLoader } from "react-spinners";
+import { Link } from "react-router-dom";
 import RestaurantDetails from "../components/RestaurantDetails";
 import useRestaurant from "../hooks/useRestaurant";
 import MenuList from "../components/MenuList";
@@ -12,12 +13,20 @@ export default function RestaurantPage() {
       <div className="bg-gray-100 min-h-screen flex items-center justify-center">
         <div className="w-full md:w-2/3 min-h-screen bg-white shadow-md rounded-md flex flex-col items-center justify-center">
           {restaurant ? (
-            <RestaurantDetails restaurant={restaurant} />
+            <>
+              <RestaurantDetails restaurant={restaurant} />
+              {restaurant.menus && <MenuList menuNames={restaurant.menus} />}
+            </>
           ) : (
             <MoonLoader size={50} />
           )}
-          {restaurant?.menus && <MenuList menuNames={restaurant.menus} />}
         </div>
+        <Link
+          to="/"
+          className="fixed bottom-4 left-4 bg-gray-200 hover:bg-gray-500 text-gray-800 hover:text-gray-200 text-xl font-extrabold px-4 py-2 rounded-full transition border-4 border-gray-300"
+        >
+          &#x2190; Back
+        </Link>
       </div>
     </>
   );
