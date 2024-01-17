eb690d43a9e493cc20a27d8195b8f58208bf0611
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-17 18:56:33 +0700

    choreL ignore env

--- packages/web/src/components/MenuModal.tsx
+++ packages/web/src/components/MenuModal.tsx
@@ -22,50 +22,56 @@ const MenuModal = ({ isOpen, menu }: MenuModalProps) => {
             className="mb-4 rounded-t-md w-full h-2/5 object-cover"
           />
         )}
-        <div className="p-4">
-          <h2 className="text-3xl font-bold mb-4">{menu.name}</h2>
-          <p className="text-lg mb-4">{menu.fullPrice}฿</p>
+        <div className="w-full h-full p-4 flex flex-col justify-between items-start">
+          <div>
+            <h2 className="text-3xl font-bold mb-4">{menu.name}</h2>
+            <p className="text-lg mb-4">{menu.fullPrice}฿</p>
 
-          {menu.discountedPercent > 0 && (
-            <p className="text-green-600 mb-4">
-              Discount: {menu.discountedPercent}% off
-            </p>
-          )}
-          {menu.discountedTimePeriod && (
-            <p className="text-gray-600 mb-4">
-              Discount valid from {menu.discountedTimePeriod.begin} to{" "}
-              {menu.discountedTimePeriod.end}
-            </p>
-          )}
+            {menu.discountedPercent > 0 && (
+              <p className="text-green-600 mb-4">
+                Discount: {menu.discountedPercent}% off
+              </p>
+            )}
+            {menu.discountedTimePeriod && (
+              <p className="text-gray-600 mb-4">
+                Discount valid from {menu.discountedTimePeriod.begin} to{" "}
+                {menu.discountedTimePeriod.end}
+              </p>
+            )}
 
-          {menu.options && menu.options.length > 0 ? (
-            <div>
-              <h3 className="text-xl font-semibold mb-2">Options:</h3>
-              <div className="mb-2">
-                <div className="list-disc pl-4">
-                  {menu.options.map((option: MenuItemOption, index: number) => (
-                    <div key={index} className="mb-2">
-                      <h4 className="text-lg font-medium text-gray-800">
-                        {option.label}
-                      </h4>
-                      <ul className="list-disc pl-4">
-                        {option.choices.map((choice, choiceIndex) => (
-                          <li key={choiceIndex} className="text-gray-600">
-                            {choice.label}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  ))}
+            {menu.options && menu.options.length > 0 ? (
+              <div>
+                <h3 className="text-xl font-semibold mb-2">Options:</h3>
+                <div className="mb-2">
+                  <div className="list-disc pl-4">
+                    {menu.options.map(
+                      (option: MenuItemOption, index: number) => (
+                        <div key={index} className="mb-2">
+                          <h4 className="text-lg font-medium text-gray-800">
+                            {option.label}
+                          </h4>
+                          <ul className="list-disc pl-4">
+                            {option.choices.map((choice, choiceIndex) => (
+                              <li key={choiceIndex} className="text-gray-600">
+                                {choice.label}
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      )
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          ) : (
-            <h3 className="text-xl font-semibold mb-2"> No option</h3>
-          )}
+            ) : (
+              <h3 className="text-xl font-semibold mb-2"> No option</h3>
+            )}
+          </div>
 
-          <div className="text-gray-800 opacity-60 text-center ">
-            Press anywhere to close
+          <div className="w-full">
+            <div className="text-gray-800 opacity-60 text-center ">
+              Press anywhere to close
+            </div>
           </div>
         </div>
       </div>
