82de8e3f428824ec4b4b119e224e37d185305983
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-18 01:33:21 +0700

    feat: add responsive to landing page

--- packages/web/src/pages/LandingPage.tsx
+++ packages/web/src/pages/LandingPage.tsx
@@ -4,11 +4,11 @@ import { restaurantList } from "../data/restaurantList";
 export default function LandingPage() {
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <div className="w-full md:w-2/3 bg-white shadow-md rounded-md p-8">
+      <div className="w-full md:w-4/5 lg:w-2/3 xl:w-2/4 bg-white shadow-md rounded-md p-8">
         <h1 className="text-3xl font-bold mb-8 text-center">
           Select a Restaurant
         </h1>
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {restaurantList.map((restaurant, index) => (
             <RestaurantCard key={index} id={restaurant.id} />
           ))}
