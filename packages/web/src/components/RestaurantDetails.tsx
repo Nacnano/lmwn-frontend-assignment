822d6d7f7950d1b10ba051c4775c9d117cd50f25
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-17 18:44:11 +0700

    feat: add open status

--- packages/web/src/components/RestaurantDetails.tsx
+++ packages/web/src/components/RestaurantDetails.tsx
@@ -5,6 +5,16 @@ interface RestaurantDetailsProps {
 }
 
 const RestaurantDetails = ({ restaurant }: RestaurantDetailsProps) => {
+  const { open, close } = restaurant.activeTimePeriod;
+
+  const isOpenNow = (): boolean => {
+    const currentTime = new Date();
+    const openTime = new Date(currentTime.toDateString() + " " + open);
+    const closeTime = new Date(currentTime.toDateString() + " " + close);
+
+    return currentTime >= openTime && currentTime <= closeTime;
+  };
+
   return (
     <div className="w-full">
       <img
@@ -14,11 +24,17 @@ const RestaurantDetails = ({ restaurant }: RestaurantDetailsProps) => {
       />
       <div className="max-w-3xl mx-4 py-4 bg-white rounded">
         <h1 className="text-3xl font-bold mb-4">{restaurant.name}</h1>
-        <h2 className="text-xl font-normal">
-          Open: {restaurant.activeTimePeriod.open}
-          {" - "}
-          {restaurant.activeTimePeriod.close}
-        </h2>
+        <div className="flex flex-wrap gap-2 justify-start items-baseline">
+          <p
+            className={`text-xl font-semibold ${isOpenNow() ? "text-green-600" : "text-red-600"}`}
+          >
+            {isOpenNow() ? "Open" : "Closed"}
+          </p>
+          <h2 className="text-lg text-gray-400 font-normal">
+            {" ⋅ "}
+            {open} - {close}
+          </h2>
+        </div>
       </div>
     </div>
   );
