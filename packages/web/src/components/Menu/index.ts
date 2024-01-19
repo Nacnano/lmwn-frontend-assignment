615f686448e389dcf9957398c17076eaf581c494
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-01-19 13:25:57 +0700

    refactor: add index file for components

---
+++ packages/web/src/components/Menu/index.ts
@@ -0,0 +1,6 @@
+export { default as LoadingMenuCard } from "./LoadingMenuCard";
+export { default as MenuCard } from "./MenuCard";
+export { default as MenuList } from "./MenuList";
+export { default as MenuModal } from "./MenuModal";
+export { default as MenuPrice } from "./MenuPrice";
+export { default as PopularMenusList } from "./PopularMenusList";
---
+++ packages/web/src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as CustomLink } from "./CustomLink";
+export { default as MobileSidebar } from "./MobileSidebar";
+export { default as NavBar } from "./NavBar";
+export { default as RangeSlider } from "./RangeSlider";
+export { default as ScrollToTopButton } from "./ScrollToTopButton";
